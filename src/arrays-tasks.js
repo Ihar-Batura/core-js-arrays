c71882b8b42dadf537eafba6880369fcb303f698
Ihar-Batura <A17331582@gmail.com>
2024-01-06 14:43:14 +0300

    feat: add code to sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let a = arr2;
+  let b = arr1;
+  if (arr1.length >= arr2.length) {
+    a = arr1;
+    b = arr2;
+  }
+  return a.map((number, index) => number + (b[index] || 0));
 }
 
 /**
