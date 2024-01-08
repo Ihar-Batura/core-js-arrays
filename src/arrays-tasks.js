80e4a5ec9f4e889106d585ba7fc00cfdc912ceb4
Ihar-Batura <A17331582@gmail.com>
2024-01-08 12:45:03 +0300

    feat: add code to createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -330,8 +330,10 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return [...Array(Math.ceil(arr.length / chunkSize))].map((_, i) =>
+    arr.slice(chunkSize * i, chunkSize + chunkSize * i)
+  );
 }
 
 /**
